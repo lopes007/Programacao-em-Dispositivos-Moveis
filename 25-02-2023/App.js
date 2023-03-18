f92ce7eb1c9734f7aceed870251c5c85132822df
lopes007 <111814271+lopes007@users.noreply.github.com>
2023-03-18 08:22:47 -0300

    Add files via upload

---
+++ 25-02-2023/App.js
@@ -0,0 +1,34 @@
+ import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import AssetExample from './components/AssetExample';
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Strogonofe com Batata Palha e Coca Cola
+      </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
